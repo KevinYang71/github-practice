033bcec94a5b1dd5786137703387d020c7076c8a
Kevin <b09901175@ntu.edu.tw>
2020-11-28 15:32:23 +0800

    finish age

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main()
-{
+
+int main(){
     string name = "";
-    cout << "Please enter your name: ";
-    cin >> name;
-    cout << "Hello, " << name << "!\n";
+    unsigned int age = 0;
+    cout << "please enter your name?" << endl;
+    cin >> name ;
+
+    cout << "hello " << name << endl;
+    cout << "how old are you?" << endl;
+    cin >> age;
+    if(!age > 0)
+        age  = 0;
+    cout << "You are " << age << " year(s) old." << endl;
     system("pause");
     return 0;
 }
